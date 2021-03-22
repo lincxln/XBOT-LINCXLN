1bbca0be2fbf486d274966676b4b2cfcb4dfd657
lincxln <72910491+lincxln@users.noreply.github.com>
2021-03-22 20:02:34 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -85,7 +85,6 @@ async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    await pong.edit("`Pinging....`")
     await pong.edit("**𖣘**")
     await pong.edit("**𖣘𖣘**")
     await pong.edit("**𖣘𖣘𖣘**")
