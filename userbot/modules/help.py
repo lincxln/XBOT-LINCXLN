eff5802f166a8310b9a272cb17ee63bf0a3ff314
lincxln <72910491+lincxln@users.noreply.github.com>
2021-03-22 16:03:26 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -35,7 +35,7 @@ async def help(event):
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\t• "
+            string += "`\t⫽ "
         await event.reply(f"⫽{string}⫽"
                           "\n╾───────────────────────╼")
         await asyncio.sleep(100)
